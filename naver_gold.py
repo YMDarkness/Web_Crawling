c015b194952725a983d16287c625f7275ef676a7
ra+ <jo000917@naver.com>
2025-04-24 22:43:59 +0900

    ARIMA 파라미터 설명 추가

--- naver_gold.py
+++ naver_gold.py
@@ -114,6 +114,22 @@ df_gold.set_index('날짜', inplace=True)
 gold_model = ARIMA(df_gold['금_시세'], order=(5, 1, 0))
 gold_model_fit = gold_model.fit()
 
+'''
+p - AR (AutoRegressive) 차수 - 과거 시계열 값 자체를 몇 시점까지 참고할지 결정해.
+                                예: p=5면 과거 5일간의 금 시세를 참고해서 오늘을 예측해.
+
+d - 차분 횟수 (Differencing) - 시계열 데이터를 몇 번 차분해서 **평균이 일정한 데이터(정상성)**로 만들지를 뜻해.
+                                예: d=1은 1차 차분을 뜻하고, 이는 오늘 값 - 어제 값을 말해
+
+q - MA (Moving Average) 차수 - 과거 예측 오차를 몇 시점까지 반영할지 설정해.
+                                예: q=0이면 오차 항은 고려하지 않겠다는 의미야.
+
+p=5: 이전 5일간 금 시세 값을 기반으로
+d=1: 데이터가 추세를 띄므로 한 번 차분하여
+q=0: 예측 오차는 반영하지 않고
+금 시세를 예측하는 모델이라는 의미
+'''
+
 #최대 5일 예측
 gold_forecast = gold_model_fit.forecast(steps=5)
 
