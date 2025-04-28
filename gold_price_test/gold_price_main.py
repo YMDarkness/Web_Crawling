c5cfa7a3c8e3295d588833db09c7df3a69a21bb2
ra+ <jo000917@naver.com>
2025-04-28 14:29:27 +0900

    금 시세 메인

--- gold_price_test/gold_price_main.py
+++ gold_price_test/gold_price_main.py
@@ -1,15 +1,15 @@
-from crawl_gold_price import gold_crwal_and_svae_csv
+from crawl_gold_price import gold_crwal_and_save_csv
 from process_gold_price import process_gold_price_csv
 from visualizer_gold_price import change_date_and_ARIMA_model
 
 def main():
-    #데이터 크롤링
-    gold_crwal_and_svae_csv()
+    #금 시세 데이터 크롤링
+    gold_crwal_and_save_csv()
     
-    #데이터 전처리
+    #금 시세 데이터 전처리
     process_gold_price_csv()
 
-    #데이터 시각화 (아리마 모델 활용)
+    #금 시세 데이터 시각화 및 예측 (아리마 모델 활용)
     change_date_and_ARIMA_model()
 
 if __name__ == '__main__':
