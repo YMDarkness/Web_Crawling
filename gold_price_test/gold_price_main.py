49743b71334f015fc9eb7a4ad05bb8327b1fc7b0
ra+ <jo000917@naver.com>
2025-06-04 21:47:10 +0900

    오타 수정 및 개선

--- gold_price_test/gold_price_main.py
+++ gold_price_test/gold_price_main.py
@@ -2,7 +2,12 @@ from crawl_gold_price import gold_crwal_and_save_csv
 from process_gold_price import process_gold_price_csv
 from visualizer_gold_price import change_date_and_ARIMA_model
 
+from pathlib import Path
+
 def main():
+    #base_path = Path(__file__).resolve().parent
+    #csv_path = base_path / 'gold_price.csv'
+
     #금 시세 데이터 크롤링
     df_gold = gold_crwal_and_save_csv()
     
@@ -11,6 +16,8 @@ def main():
 
     #금 시세 데이터 시각화 및 예측 (아리마 모델 활용)
     df_gold = change_date_and_ARIMA_model(df_gold)
+
+    #df_gold.to_csv(csv_path, index=False)
 
 if __name__ == '__main__':
     main()
