47241e7cebea3a423b7e781d187ee2aadee545fb
ra+ <jo000917@naver.com>
2025-06-09 21:52:54 +0900

    코스피 지수 크롤링

--- kospi_test/kospi_XGBClassifier.py
+++ kospi_test/kospi_XGBClassifier.py
@@ -64,16 +64,14 @@ def kospi_model_upgrade(df_kospi, model_type='XGB'):
     #예측 / 평가
     preds = model.predict(X_test)
     mse = mean_squared_error(Y_test, preds)
-    #rmse = mean_squared_error(Y_test, preds)
     rmse = np.sqrt(mse)
     mae = mean_absolute_error(Y_test, preds)
-    Y_test = Y_test.replace(0, np.nan)
-    maps = (abs((Y_test - preds) / Y_test) * 100).mean()
-    maps = (abs((Y_test - preds) / Y_test.replace(0, np.nan)) * 100).mean()
+    Y_test_nonzero = Y_test.replace(0, np.nan)  # 0으로 나누는 것을 방지하기 위해 0을 NaN으로 대체
+    mape = (abs((Y_test_nonzero - preds) / Y_test_nonzero) * 100).mean()
 
     print(f'[알람] {model_type.upper()} 모델 RMSE: {rmse:.2f}')
     print(f'[알람] {model_type.upper()} 모델 MAE: {mae:.2f}')
-    print(f'[알람] {model_type.upper()} 모델 MAPE: {maps:.2f} \n')
+    print(f'[알람] {model_type.upper()} 모델 MAPE: {mape:.2f} \n')
 
     #시각화
     '''plt.plot(Y_test.values, label='실제값')
@@ -90,11 +88,26 @@ def kospi_model_upgrade(df_kospi, model_type='XGB'):
     print("\n [알람] LightGBM 모델 학습 및 평가")
     #_, model_lgb, X_test_lgb, Y_test_lgb, preds_lgb = kospi_model_upgrade(df_kospi, model_type='lgb')
 
-    # 예측 비교 시각화
-    plt.figure(figsize=(12, 6))
-    plt.plot(Y_test.values, label='실제 종가 (5일 후)', color='blue')
-    plt.plot(preds, label='XGBoost 예측', color='orange', linestyle='--')
-    plt.plot(preds, label='LightGBM 예측', color='green', linestyle=':')
+    print('\n')
+
+    return df_kospi, {
+        'model' : model,
+        'X_test' : X_test,
+        'Y_test' : Y_test,
+        'preds' : preds,
+        'rmse' : rmse,
+        'mae' : mae,
+        'mape' : mape
+    }
+
+def visualize_graph(df_kospi, result_xgb, result_lgb):
+    df_kospi = bollinger_kospi(df_kospi)
+
+    #시각화
+    plt.figure(figsize=(14, 7))
+    plt.plot(result_xgb['Y_test'].values, label='실제값 (XGB)', color='blue')
+    plt.plot(result_xgb['preds'], label='예측값 (XGB)', color='orange', linestyle='--')
+    plt.plot(result_lgb['preds'], label='예측값 (LGB)', color='green', linestyle=':')
     plt.title('코스피 지수 예측 비교 : XGBoost vs LightGBM')
     plt.xlabel('샘플')
     plt.ylabel('종가')
@@ -102,21 +115,3 @@ def kospi_model_upgrade(df_kospi, model_type='XGB'):
     plt.grid(True)
     plt.tight_layout()
     plt.show()
-
-    print('\n')
-
-    return {
-        'df_kospi': df_kospi,
-        'xgb': {
-            'model': model,
-            'X_test': X_test,
-            'Y_test': Y_test,
-            'preds': preds,
-        },
-        'lgb': {
-            'model': model,
-            'X_test': X_test,
-            'Y_test': Y_test,
-            'preds': preds,
-        }
-    }
