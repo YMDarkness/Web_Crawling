833f58cd0ba7ea6be2569381f17bff2b0f502081
ra+ <jo000917@naver.com>
2025-06-12 20:10:34 +0900

    전체 실행 스크립트

---
+++ market_index_monitoring/market_index_monitoring.py
@@ -0,0 +1,70 @@
+from modules.market_collector import MarketCollector
+from modules.gold_collector import GoldCollector
+from modules.news_collector import NewsCollector
+from modules.kospi_collector import KospiCollector
+from modules.dji_collector import DJICollector
+from modules.n225_collector import N225Collector
+from modules.sp_collector import SpCollector
+from modules.ixic_collector import IXICCollector
+
+# main
+
+def main():
+    # 환율
+    market = MarketCollector()
+    market.fetch()
+    market.parse()
+    
+    print(market.to_prometheus_format())
+
+    # 금 시세
+    gold = GoldCollector()
+    gold.fetch()
+    gold.parse()
+    
+    print(gold.to_prometheus_format())
+
+    # 증권 뉴스
+    news = NewsCollector()
+    news.fetch()
+    news.parse()
+
+    print(news.to_prometheus_format())
+
+    # kospi
+    kospi = KospiCollector()
+    kospi.fetch()
+    kospi.parse()
+
+    print(kospi.to_prometheus_format())
+
+    # s&p500
+    sp = SpCollector()
+    sp.fetch()
+    sp.parse()
+
+    print(sp.to_prometheus_format())
+
+    # 나스닥
+    ixic = IXICCollector()
+    ixic.fetch()
+    ixic.parse()
+
+    print(ixic.to_prometheus_format())
+
+    # 다우존스
+    dji = DJICollector()
+    dji.fetch()
+    dji.parse()
+
+    print(dji.to_prometheus_format())
+
+    # 니케이225
+    n225 = N225Collector()
+    n225.fetch()
+    n225.parse()
+
+    print(n225.to_prometheus_format())
+
+if __name__ == "__main__":
+    main()
