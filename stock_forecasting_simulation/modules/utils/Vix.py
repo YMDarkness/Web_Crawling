b3b8a9299aa81f2a3a01e9984e28eb5c0bab762b
ra+ <jo000917@naver.com>
2025-07-09 16:07:58 +0900

    변동성 지수

--- stock_forecasting_simulation/modules/utils/Vix.py
+++ stock_forecasting_simulation/modules/utils/Vix.py
@@ -11,18 +11,29 @@ class VIX(BaseCrawler):
 
     # 수집 대상 웹페이지 요청
     def fetch_data(self):
-        self.html = requests.get('').text
+        # self.html = requests.get('').text
+        url = 'https://m.stock.naver.com/worldstock/index/.VIX/total'
+        wait_selector = (By.CLASS_NAME, 'GraphMain_price__H72B2')
+        super().fetch_data(url, wait_selector)
 
     def parse_data(self):
         soup = BeautifulSoup(self.html, 'html.parser')
-        vix_price = soup.select_one('')
+        vix_price = soup.select_one('strong.GraphMain_price__H72B2')
+        '''
         self.data = {
             'vix_price' : float(vix_price.text.replace(',', '').replace('원', '')) if vix_price else 0.0
         }
+        '''
+        price = float(
+            vix_price.text.strip().split('USD')[0].replace(',', '')
+        ) if vix_price else 0.0
+        self.data = {'vix_price' : price}
 
+    '''
     def prometheus_format(self):
         lines = []
         for key, value in self.data.items():
             metric = f'vix_{key}'.replace('.', '')
             lines.append(f'{metric} {value}')
         return '\n'.join(lines) + '\n'
+    '''
