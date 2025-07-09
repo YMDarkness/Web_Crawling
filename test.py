45456d381e612ca96ab6793f155d7ec96402b2c0
ra+ <jo000917@naver.com>
2025-07-09 16:05:50 +0900

    크롤링 테스트용

--- test.py
+++ test.py
@@ -6,7 +6,7 @@ import requests
 from bs4 import BeautifulSoup
 import time
 
-url = 'https://upbit.com/exchange?code=CRIX.UPBIT.KRW-BTC&utm_source=naver&utm_medium=SA&utm_campaign=up_pc&utm_group=%EC%A3%BC%EC%9A%94%EC%A2%85%EB%AA%A9%EB%AA%85&utm_term=%EB%B9%84%ED%8A%B8%EC%BD%94%EC%9D%B8&n_media=27758&n_query=%EB%B9%84%ED%8A%B8%EC%BD%94%EC%9D%B8&n_rank=2&n_ad_group=grp-a001-01-000000051195453&n_ad=nad-a001-01-000000389016399&n_keyword_id=nkw-a001-01-000007235860525&n_keyword=%EB%B9%84%ED%8A%B8%EC%BD%94%EC%9D%B8&n_campaign_type=1&n_ad_group_type=1&n_match=1'
+url = ''
 
 options = Options()
 options.add_argument('--headless')
