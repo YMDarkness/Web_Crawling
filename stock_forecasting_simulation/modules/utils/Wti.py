a6d8ffc568a29b550e7ebda1819d678580bf4a21
ra+ <jo000917@naver.com>
2025-07-10 20:31:32 +0900

    WTI (서부 텍사스 중질유) 시세

--- stock_forecasting_simulation/modules/utils/Wti.py
+++ stock_forecasting_simulation/modules/utils/Wti.py
@@ -11,18 +11,29 @@ class WTI(BaseCrawler):
 
     # 수집 대상 웹페이지 요청
     def fetch_data(self):
-        self.html = requests.get('https://finance.naver.com/marketindex/worldOilDetail.naver?marketindexCd=OIL_CL&fdtc=2').text
+        # self.html = requests.get('https://finance.naver.com/marketindex/worldOilDetail.naver?marketindexCd=OIL_CL&fdtc=2').text
+        url = 'https://finance.naver.com/marketindex/worldOilDetail.naver?marketindexCd=OIL_CL&fdtc=2'
+        wait_selector = (By.CLASS_NAME, 'no_today')
+        super().fetch_data(url, wait_selector)
 
     def parse_data(self):
         soup = BeautifulSoup(self.html, 'html.parser')
-        wti_price = soup.select_one('#content > div.spot > div.today > p.no_today')
+        wti_price = soup.select_one('p.no_today')
+        '''
         self.data = {
             'wti_price': float(wti_price.text.replace(',', '').replace('원', '')) if wti_price else 0.0
         }
+        '''
+        price = float(
+            wti_price.text.strip().split('KRW')[0].replace(',', '')
+        ) if wti_price else 0.0
+        self.data = {'wti_price' : price}
 
+    '''
     def prometheus_format(self):
         lines = []
         for key, value in self.data.items():
             metric = f'wti_{key}'.replace('.', '')
             lines.append(f'{metric} {value}')
         return '\n'.join(lines) + '\n'
+    '''
