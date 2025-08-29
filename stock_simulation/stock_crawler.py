546b470bba6a2c69816548f881b8dc1f364416fd
ra+ <jo000917@naver.com>
2025-08-29 19:37:09 +0900

    주가 크롤링

--- stock_simulation/stock_crawler.py
+++ stock_simulation/stock_crawler.py
@@ -11,16 +11,20 @@ import time
 
 # 크롤링
 
-def crawl_recent_price(stock_code: str, days: int = 30) -> pd.DataFrame:
+def crawl_recent_price(stock_code: str, days: int = 365) -> pd.DataFrame:
     '''
     네이버 금융에서 지정한 종목코드의 최근 30일간 주가 데이터를 수집
+    -> 예측 모델의 정확도를 위해 30일에서 1년으로 기간을 변경
 
     Args:
         stock_code (str): 종목 코드 (예: '005930' 삼성전자)
-        days (int): 수집할 일수 (기본값: 30일)
+        days (int): 수집할 일수 (기본값: 30일) -> (기본값: 365일)
 
     Returns:
         pd.DataFrame: 날짜, 종가, 시가, 고가, 저가, 거래량을 포함하는 데이터프레임
+
+    from bs4 import BeautifulSoup가 아닌 selenium인 이유
+    -> 동적 페이지 로딩이 필요한 경우가 있기 때문
     '''
 
     base_url = f'https://finance.naver.com/item/sise_day.nhn?code={stock_code}'
