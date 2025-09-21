c84290d0c324cb1e5f1279ab675d460af487b1f1
ra+ <jo000917@naver.com>
2025-09-21 19:21:01 +0900

    테슬라 주가

--- stock_forecasting_simulation/modules/utils/Tesla.py
+++ stock_forecasting_simulation/modules/utils/Tesla.py
@@ -1,7 +1,10 @@
 import requests
+import re
 from bs4 import BeautifulSoup
 from selenium.webdriver.common.by import By
+
 from .BaseCrawler import BaseCrawler
+from .price_parser import extract_price
 
 # 테슬라 주가
 
@@ -19,22 +22,10 @@ class Tesla(BaseCrawler):
     def parse_data(self):
         soup = BeautifulSoup(self.html, 'html.parser')
         tesla_price = soup.select_one('strong.GraphMain_price__H72B2')
-        '''
-        self.data = {
-            'tesla_price' : float(tesla_price.text.replace(',', '').replace('원', '')) if tesla_price else 0.0
-        }
-        '''
-        price = float(
-            tesla_price.text.strip().split('USD')[0].replace(',', '')
-        ) if tesla_price else 0.0
-        self.data = {'tesla_price' : price}
+        
+        price = 0.0
+        if tesla_price:
+            price = extract_price(tesla_price.text)
 
-    '''
-    def prometheus_format(self):
-        lines = []
-        for key, value in self.data.items():
-            metric = f'tesla_{key}'.replace('.', '')
-            lines.append(f'{metric} {value}')
-        return '\n'.join(lines) + '\n'
-    '''
-    
+        self.data = {'tesla_price' : price}
+        
