7fc66681a6316eab8d4393f9543f6a1cdcbb176c
ra+ <jo000917@naver.com>
2025-09-21 19:19:45 +0900

    엘지 에너지솔루션 주가

--- stock_forecasting_simulation/modules/utils/LGensol.py
+++ stock_forecasting_simulation/modules/utils/LGensol.py
@@ -1,7 +1,10 @@
 import requests
+import re
 from bs4 import BeautifulSoup
 from selenium.webdriver.common.by import By
+
 from .BaseCrawler import BaseCrawler
+from .price_parser import extract_price
 
 # LG 에너지솔루션 주가
 
@@ -19,22 +22,9 @@ class LGensol(BaseCrawler):
     def parse_data(self):
         soup = BeautifulSoup(self.html, 'html.parser')
         lgensol_price = soup.select_one('p.no_today')
-        '''
-        self.data = {
-            'lgensol_price' : float(lgensol_price.text.replace(',', '').replace('원', '')) if lgensol_price else 0.0
-        }
-        '''
-        price = float(
-            lgensol_price.text.strip().split('KRW')[0].replace(',', '').replace('\n', '')
-        ) if lgensol_price else 0.0
-        self.data = {'lgensol_price' : price}
+        
+        price = 0.0
+        if lgensol_price:
+            price = extract_price(lgensol_price.text)
 
-    '''
-    def prometheus_format(self):
-        lines = []
-        for key, value in self.data.items():
-            metric = f'lgensol_{key}'.replace('.', '')
-            lines.append(f'{metric} {value}')
-        return '\n'.join(lines) + '\n'
-    '''
-    
+        self.data = {'lgensol_price' : price}
