595ab8cb2d4955287bc89a070915e0f049debe2b
ra+ <jo000917@naver.com>
2025-09-21 19:16:07 +0900

    애플 주가

--- stock_forecasting_simulation/modules/utils/Apple.py
+++ stock_forecasting_simulation/modules/utils/Apple.py
@@ -1,7 +1,10 @@
 import requests
+import re
 from bs4 import BeautifulSoup
 from selenium.webdriver.common.by import By
+
 from .BaseCrawler import BaseCrawler
+from .price_parser import extract_price
 
 # 애플 주가
 
@@ -19,23 +22,10 @@ class Apple(BaseCrawler):
     def parse_data(self):
         soup = BeautifulSoup(self.html, 'html.parser')
         apple_price = soup.select_one('strong.GraphMain_price__H72B2')
-        '''
-        self.data = {
-            'apple_price' : float(apple_price.text.replace(',', '').replace('원', '')) if apple_price else 0.0
-        }
-        '''
-        price = float(
-            apple_price.text.strip().split('USD')[0].replace(',', '')
-        ) if apple_price else 0.0
+        
+        price = 0.0
+        if apple_price:
+            price = extract_price(apple_price.text)
+
         self.data = {'apple_price' : price}
-    
-    '''
-    def prometheus_format(self):
-        lines = []
-        for key, value in self.data.items():
-            metric = f'apple_{key}'.replace('.', '') # 지표 이름 생성
-            lines.append(f'{metric} {value}') # Prometheus 포맷 생성
-        return '\n'.join(lines) + '\n' # 한 줄씩 붙이기
-    # 크롤링한 데이터를 Prometheus가 이해할 수 있는 텍스트 포맷으로 변환하는 역할
-    '''
     
