e75c773788c2c81d50c5cdebce9a3164320307a6
ra+ <jo000917@naver.com>
2025-09-21 19:20:13 +0900

    SK 하이닉스 주가

--- stock_forecasting_simulation/modules/utils/SKHynix.py
+++ stock_forecasting_simulation/modules/utils/SKHynix.py
@@ -1,7 +1,10 @@
 import requests
+import re
 from bs4 import BeautifulSoup
 from selenium.webdriver.common.by import By
+
 from .BaseCrawler import BaseCrawler
+from .price_parser import extract_price
 
 # SK하이닉스 주가
 
@@ -19,22 +22,9 @@ class SKHynix(BaseCrawler):
     def parse_data(self):
         soup = BeautifulSoup(self.html, 'html.parser')
         skhynix_price = soup.select_one('p.no_today')
-        '''
-        self.data = {
-            'skhynix_price' : float(skhynix_price.text.replace(',', '').replace('원', '')) if skhynix_price else 0.0
-        }
-        '''
-        price = float(
-            skhynix_price.text.strip().split('KRW')[0].replace(',', '')
-        ) if skhynix_price else 0.0
+        
+        price = 0.0
+        if skhynix_price:
+            price = extract_price(skhynix_price.text)
+
         self.data = {'skhynix_price' : price}
-    
-    '''
-    def prometheus_format(self):
-        lines = []
-        for key, value in self.data.items():
-            metric = f'skhynix_{key}'.replace('.', '')
-            lines.append(f'{metric} {value}')
-        return '\n'.join(lines) + '\n'
-    '''
-    
