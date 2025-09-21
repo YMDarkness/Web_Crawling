a66afd842d5c3846b55ab8d5d3f6e7abf44d71ba
ra+ <jo000917@naver.com>
2025-09-21 19:22:54 +0900

    네이버 주가

--- stock_forecasting_simulation/modules/utils/Naver.py
+++ stock_forecasting_simulation/modules/utils/Naver.py
@@ -1,7 +1,10 @@
 import requests
+import re
 from bs4 import BeautifulSoup
 from selenium.webdriver.common.by import By
+
 from .BaseCrawler import BaseCrawler
+from .price_parser import extract_price
 
 # NAVER 주가
 
@@ -19,22 +22,10 @@ class Naver(BaseCrawler):
     def parse_data(self):
         soup = BeautifulSoup(self.html, 'html.parser')
         naver_price = soup.select_one('p.no_today')
-        '''
-        self.data = {
-            'naver_price' : float(naver_price.text.replace(',', '').replace('원', '')) if naver_price else 0.0
-        }
-        '''
-        price = float(
-            naver_price.text.strip().split('KRW')[0].replace(',', '')
-        ) if naver_price else 0.0
-        self.data = {'naver_price' : price}
+        
+        price = 0.0
+        if naver_price:
+            price = extract_price(naver_price.text)
 
-    '''
-    def prometheus_format(self):
-        lines = []
-        for key, value in self.data.items():
-            metric = f'naver_{key}'.replace('.', '')
-            lines.append(f'{metric} {value}')
-        return '\n'.join(lines) + '\n'
-    '''
+        self.data = {'naver_price' : price}
     
