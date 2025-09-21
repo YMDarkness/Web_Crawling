d9ec806244437a6b0f778d42648af7a22a0913ba
ra+ <jo000917@naver.com>
2025-09-21 19:17:16 +0900

    버크셔 해서웨이 주가

--- stock_forecasting_simulation/modules/utils/BerkshireHathaway.py
+++ stock_forecasting_simulation/modules/utils/BerkshireHathaway.py
@@ -1,7 +1,10 @@
 import requests
+import re
 from bs4 import BeautifulSoup
 from selenium.webdriver.common.by import By
+
 from .BaseCrawler import BaseCrawler
+from .price_parser import extract_price
 
 # 버크셔 헤서웨이 주가
 
@@ -19,22 +22,9 @@ class BerkshireHathaway(BaseCrawler):
     def parse_data(self):
         soup = BeautifulSoup(self.html, 'html.parser')
         berkshire_price = soup.select_one('strong.GraphMain_price__H72B2')
-        '''
-        self.data = {
-            'berkshire_price': float(berkshire_price.text.replace(',', '').replace('원', '')) if berkshire_price else 0.0
-        }
-        '''
-        price = float(
-            berkshire_price.text.strip().split('USD')[0].replace(',', '')
-        ) if berkshire_price else 0.0
-        self.data = {'berkshire_price' : price}
-    
-    '''
-    def prometheus_format(self):
-        lines = []
-        for key, value in self.data.items():
-            metric = f'berkshire_{key}'.replace('.', '')
-            lines.append(f'{metric} {value}')
-        return '\n'.join(lines) + '\n'
-    '''
-    
+        
+        price = 0.0
+        if berkshire_price:
+            price = extract_price(berkshire_price.text)
+
+        self.data = {'berkshire_hathaway_price' : price}
